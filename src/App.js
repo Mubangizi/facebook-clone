6278d530d17aa4081cc7e65ada8a82420e2014cb
Mubangizi1 <Mubangizi1@gitlab.com>
2020-09-11 11:56:58 +0300

    Create initial react canvas

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="app">
+      <hi>Lets build the facebook clone</hi>
+    </div>
+  );
+}
+
+export default App;
