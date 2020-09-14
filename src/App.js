e7ed0e5b87ba7b3e5d8e1a46a3326898633dd338
Mubangizi1 <Mubangizi1@gitlab.com>
2020-09-14 09:27:40 +0300

    Add story Reel

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import SideBar from "./SideBar";
+import Feed from "./Feed";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="app__body">
       <SideBar />
-      {/* App body */}
-      {/* Side bar */}
-      {/* Feed */}
+      <Feed />
       {/* Widgets */}
+      </div>
     </div>
   );
 }
---
+++ src/StoryReel.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./StoryReel.css";
+
+function StoryReel() {
+  return (
+    <div className="storyReel">
+      <story image="" profileSrc="" title="" />
+      <story image="" profileSrc="" title="" />
+      <story image="" profileSrc="" title="" />
+      <story image="" profileSrc="" title="" />
+      <story image="" profileSrc="" title="" />
+    </div>
+  );
+}
+
+export default StoryReel;
