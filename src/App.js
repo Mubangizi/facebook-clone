4d4e905fd0eb7c0bb5e45bf881f5aa94987600fd
Mubangizi1 <Mubangizi1@gitlab.com>
2020-09-23 16:51:55 +0300

    Add fb widget

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header";
 import SideBar from "./SideBar";
 import Feed from "./Feed";
+import Widgets from "./Widgets";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div className="app__body">
       <SideBar />
       <Feed />
-      {/* Widgets */}
+      <Widgets/>
       </div>
     </div>
   );
